7e77ab0c2a98ea28b8fa12954b4c48efebf2e455
trophime1 <trophime250@gmail.com>
2022-06-16 16:27:49 +0200

    final

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -23,6 +23,8 @@ function Footer()
       </div>
     </div>
   </footer>
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
         </>
     );
 }
