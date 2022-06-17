490acfc495569686a4b8e7326e3766b6aff5ef93
trophime1 <trophime250@gmail.com>
2022-06-17 16:37:41 +0200

    main.js

--- src/App.js
+++ src/App.js
@@ -10,10 +10,48 @@ import Testimony from './pages/testimony';
 import Blog from './pages/blog';
 import Contact from './pages/contact';
 import Footer from './pages/footer';
-import './public/assets/js/main'
+import { useEffect } from 'react';
 import {BrowserRouter as Router ,Route ,Routes} from 'react-router-dom';
+import ScriptTag from 'react';
 
 function App() {
+  const scrollWithHash =() => {
+		if (window.location.hash) {
+			if (document.querySelector(window.location.hash)) {
+				let header = document.getElementById('header');
+				let offset = header.offsetHeight;
+				let elementPos = document.querySelector(window.location.hash).offsetTop;
+				if (!header.classList.contains('header-scrolled')) {
+					offset -= 10;
+				}
+
+				window.scrollTo({
+					top: elementPos - offset,
+					behavior: 'smooth'
+				});
+			}
+		}
+	}
+
+	useEffect(() => {
+		var c = Array.from(document.getElementsByTagName('a'));
+		c.forEach((element) => {
+			element.addEventListener('click', () => {
+				window.scroll(0, 0);
+			});
+		});
+
+		const script = document.createElement('script');
+		script.src = "/assets/js/main.js";
+		script.async = true;
+
+		document.body.appendChild(script);
+		scrollWithHash();
+
+		return () => {
+			document.body.removeChild(script);
+		}
+	}, []);
   return (
     <>
 
