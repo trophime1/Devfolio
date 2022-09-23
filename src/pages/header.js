4a7aaa7411d7145e1a51548c8afbc35831ac13fc
trophime karasisi <88462868+trophime1@users.noreply.github.com>
2022-09-23 10:30:03 +0200

    errors to be corrected

--- src/pages/header.js
+++ src/pages/header.js
@@ -31,21 +31,7 @@ useEffect( ()=>{
           <li><a className="nav-link scrollto " href="#work">Work</a></li>
           <li><a className="nav-link scrollto " href="#blog">Blog</a></li>
           <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
+            
           </li>
           <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
         </ul>
