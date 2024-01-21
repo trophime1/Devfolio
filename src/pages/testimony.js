dbd852cfbdacd7d39cfcf5538f139901d09b34f1
trophime1 <trophime250@gmail.com>
2024-01-21 14:14:26 +0200

    tesmonia

--- src/pages/testimony.js
+++ src/pages/testimony.js
@@ -2,7 +2,7 @@ import React from "react";
 function Testimony () {
     return(
         <>
-         {/* <div className="testimonials paralax-mf bg-image" style={{backgroundImage: "url(assets/img/overlay-bg.jpg)"}}>
+         <div className="testimonials paralax-mf bg-image" style={{backgroundImage: "url(assets/img/danny2.png)"}}>
       <div className="overlay-mf"></div>
       <div className="container">
         <div className="row">
@@ -14,13 +14,17 @@ function Testimony () {
                 <div className="swiper-slide">
                   <div className="testimonial-box">
                     <div className="author-test">
-                      <img src="assets/img/testimonial-2.png" alt="" className="rounded-circle b-shadow-a"/>
-                      <span className="author">Xavi Alonso</span>
+                      {/* <img src="assets/img/danny.png" alt="" className="rounded-circle b-shadow-a"/> */}
+                      <span className="author">Daniel HAGENIMANA</span>
                     </div>
                     <div className="content-test">
                       <p className="description lead">
-                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit.
+                      Niba ushaka gukora ikintu uzabana nacyo cyangwa kizakugirira umumaro mugihe kirekire uzafate umwanzuro 
+                      mugihe ubabajwe nacyo cyangwa uhangayikishijwe nacyo. (Nujya gushaka umugore cyangwa umugabo 
+                      uzafate umwanzuro mugihe washwanye nawe byanyabyo ufate ibyiza bye ugereranye nibibi ubone gufata 
+                      umwanzuro , Nujya gukora business izakugirira umumaro mugihe kinini uzafate umwanzuro mugihe wahombye 
+                      muri business ntoya wakoraga ibyo biguha imbaraga zo gutsinda byose wahura nabyo mubuzima cyangwa muri business.)
+
                       </p>
                     </div>
                   </div>
@@ -30,12 +34,76 @@ function Testimony () {
                   <div className="testimonial-box">
                     <div className="author-test">
                       <img src="assets/img/testimonial-4.jpg" alt="" className="rounded-circle b-shadow-a"/>
-                      <span className="author">Marta Socrate</span>
+                      <span className="author">Daniel HAGENIMANA</span>
                     </div>
                     <div className="content-test">
                       <p className="description lead">
-                        Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Lorem ipsum dolor sit amet,
-                        consectetur adipiscing elit.
+                      Nujya gukora ikintu ukabona buriwese arikubisobanukirwa uzamenye ko ibyo bizagufasha kubona ibyo kurya gusa 
+                      naho nubona benshi batabyumva neza uzamenyeko ibyo bizakugeza kurehashoboka ukaba wagaburira nigihugu.
+                      Ntago zahabu ibonwa naburiwese niyo mpamvu akenshi abantu baba barageze kubintu bihambaye usanga baba 
+                      barirengagijwe kenshi ntawabonaga icyo baribashoboye bibaho kugeza  undi ufite ubwenge bungana nazahabu abonye yayindi iri muri wamuntu hanyuma akayibyaza umusaruro. 
+
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="swiper-slide">
+                  <div className="testimonial-box">
+                    <div className="author-test">
+                      <img src="assets/img/testimonial-4.jpg" alt="" className="rounded-circle b-shadow-a"/>
+                      <span className="author">Daniel HAGENIMANA</span>
+                    </div>
+                    <div className="content-test">
+                      <p className="description lead">
+                      Biragoyeko buriwese yakumva ayamagambo akomeye gutya koyamuhindurira ubuzima gusa kuri 
+                      ngewe nicyo gipimo kinyerekako ntari 
+                      uwariwe wese , bake babasha kubyumva byahindura ubuzima bwabo.
+                      Gukora noguhuza igihe cyawe nibintu byingenzi benshi babivamo bataragera 
+                      kunsinzi kuberko bishyiriraho igihe cyabo , gusa iyo ubashishe gukora utitaye 
+                      kugihe igihe kikwitaho kikakugenderaho.
+
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="swiper-slide">
+                  <div className="testimonial-box">
+                    <div className="author-test">
+                      <img src="assets/img/testimonial-4.jpg" alt="" className="rounded-circle b-shadow-a"/>
+                      <span className="author">Daniel HAGENIMANA</span>
+                    </div>
+                    <div className="content-test">
+                      <p className="description lead">
+                      Leta zibihugu bya africa zikwiye gushyira imbaraga mu guhanga imirimo 
+                      ndetse nabikorera niba bifuza kudusigira ibihugu tuzabamo ntamyenda 
+                      yabanyaburayi nabuburengerazuba bwisi , dukwiye gushinga ama company 
+                      duteganya ko azagera kundunduro yintego zacu byibuze mumyaka 50 irimbere
+                       aho niho tuzaba dukoze ishoramari ryurubyiruko ndetse nabana bacu bitabaye
+                        ibyo ntibazongere kuvuga ubwigenge  nge ntabwo mbona. ikibabaje abagakoze
+                         ibyo bajya kubakisha ayomafaranga  yabaturagr imiturirwa muri byabihugu bikize 
+                         byibuze ntibanazubake inaha mubihugu bikennye ngobaduhe akazi ko kuba abayedi naba fundi
+                       ahubwo imitungo yibihugu bakayi jyana iyo hanze gufasha abifashije.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="swiper-slide">
+                  <div className="testimonial-box">
+                    <div className="author-test">
+                      <img src="assets/img/testimonial-4.jpg" alt="" className="rounded-circle b-shadow-a"/>
+                      <span className="author">Daniel HAGENIMANA</span>
+                    </div>
+                    <div className="content-test">
+                      <p className="description lead">
+                      Sinzi niba naba ndumusazi ndamutse mvuzeko america nuburayi bakwiye kutwishyura amafaranga abasogukuru bacu babanyafurica 
+                      barenga miliyoni 40 bakoreye badahembwa mumirimo yagahato , ariko banyita umusazi byihuse ndamutse mvuzeko inzirakarengane 
+                      zabanya africa muri rusange abanya Libya , Iraq, siriya bakwiye ubutabera , burya ukuri ntago ari ukuri nkuko byumvikana ahubwo 
+                      ukuri nuruhande ruriho abanya bubasha gusa amahirwe ntago ububasha buhora hamwe ubwo ukuri nako guhora kwimuka.
+                      Abari kwiga sibo bagakwiye kwiga  ahubwo abadakwiye kwiga usanga bari kwigishwa kwiyakira mubuzima bubi barimo kubera kuyoborwa 
+                      nabadafite ubushobozi buhagije bwimitekerereze ahubwo bashyira imbere ikenewabo , akenshi muri africa abafite power usanga 
+                      ntabumenyi ariko abafite ubumenyi ugasanga nta power bafite , ndushyiraho ubuyobizi nagira wawundi ubasha guha inkoko imwe mumwaka 
+                      akaguha ihene minstri wimari kurusha wawundi umufite amashuri ahanitse utabasha nagato kugabanya miliyari mubaturage ngo abahe
+                      inka neza bikure mubukene.
                       </p>
                     </div>
                   </div>
@@ -50,7 +118,7 @@ function Testimony () {
           </div>
         </div>
       </div>
-      </div> */}
+      </div>
         </>
     );
 } 
